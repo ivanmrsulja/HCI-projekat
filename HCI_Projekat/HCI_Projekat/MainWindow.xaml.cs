33e3ee94f6034e44dcb56e3c54510202acea612f
Ivan <nighteliteace@gmail.com>
2021-04-18 11:23:25 +0200

    Inicijalni setup projekta, dodata login forma.

---
+++ HCI_Projekat/HCI_Projekat/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HCI_Projekat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void UlogujSe(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Logovanje");
+        }
+
+        public void RegistrujSe(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Registracija");
+        }
+    }
+
+}
